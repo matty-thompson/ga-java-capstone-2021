44d50b1a5a3bd45fe898fb684ac434f09de2e57d
Matty Thompson <matthompson@paypal.com>
2021-11-12 15:49:10 -0600

    Created help intent handler

--- src/main/java/com/example/alexaskillcongresswatch/handler/HelpIntentHandler.java
+++ src/main/java/com/example/alexaskillcongresswatch/handler/HelpIntentHandler.java
@@ -2,27 +2,25 @@ package com.example.alexaskillcongresswatch.handler;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
-import com.amazon.ask.model.LaunchRequest;
 import com.amazon.ask.model.Response;
-import com.amazon.ask.request.Predicates;
+import static com.amazon.ask.request.Predicates.intentName;
 
 import java.util.Optional;
 
-public class LaunchRequestHandler implements RequestHandler {
+public class HelpIntentHandler implements RequestHandler {
 
     @Override
     public boolean canHandle(HandlerInput input) {
-        return input.matches(Predicates.requestType(LaunchRequest.class));
+        return input.matches(intentName("AMAZON.HelpIntent"));
     }
 
     @Override
     public Optional<Response> handle(HandlerInput input) {
-        String speechText = "Welcome to Congress Watch";
+        String speechText = "You can ask me about your congress people!";
         return input.getResponseBuilder()
                 .withSpeech(speechText)
                 .withSimpleCard("CongressWatch", speechText)
                 .withReprompt(speechText)
                 .build();
     }
-
 }
